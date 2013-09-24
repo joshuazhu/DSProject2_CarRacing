942b215d758386b0e1895575e630332ff14c5b04
joshua <joshuazhu1989@gmail.com>
2013-09-24 12:51:33 +1000

    Upload Images

---
+++ DSProj1/src/view/Images.java
@@ -0,0 +1,20 @@
+package view;
+
+import javafx.scene.image.Image;
+
+public class Images
+{
+  //private static Image map = new Image("/resources/images/prologue.png");
+ 
+
+  public static Image getSniper()
+  {
+    return sniper;
+  }
+
+}
+
+/* Location:           C:\Users\Josh\Desktop\beta.jar
+ * Qualified Name:     view.Images
+ * JD-Core Version:    0.6.2
+ */
